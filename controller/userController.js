a35b8dfbe9aa8ba2f687f1ae6ddad78362470ab8
Nandan Choudhary <nandanchoudhry8822@gmail.com>
2024-08-06 21:40:16 +0530

    tolist

---
+++ controller/userController.js
@@ -0,0 +1,20 @@
+import user from "../model/userModel.js";
+
+
+export const create = async(req, res) =>{
+    try{
+        const newUser = new User(req.body);
+        const {email} = newUser;
+
+        const userExist = await User.findOne({email})
+        if(userExist){
+            return res.status(400).json({message : "user already exists."})
+        }
+        const savedData = await newUser.save();
+        res.status(200).json(savedData);
+
+    }catch (error) {
+        res.status(500).json({errorMessage:error.message})
+        }
+     
+}

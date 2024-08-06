344a8ea18e0e0da6715a94004412960e902052c4
Nandan Choudhary <nandanchoudhry8822@gmail.com>
2024-08-06 21:53:38 +0530

    djhhfew

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express from "express"
-import mongoose from "mongoose"
-import bodyParser from "body-parser"
-import dotenv from "dotenv"
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 import route from "./routes/userRoute.js";
 
 const app = express();
@@ -12,13 +12,13 @@ const PORT = process.env.PORT || 7000;
 const MONGOURL = process.env.MONGO_URL;
 
 mongoose
-        .connect(MONGOURL)
-        .then(()=>{
-                console.log("DB connected successfully.");
-                app.listen(PORT,()=>{
-                      console.log(`server is running on port :${PORT}`);
-                })
-        })
-        .catch((error)=> console.log(error));
+  .connect(MONGOURL)
+  .then(() => {
+    console.log("hvferv");
+    app.listen(PORT, () => {
+      console.log(`server is running on port :${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
 
-        app.use("/api", route);
+app.use("/api", route);
